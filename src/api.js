db5c09171bc5be2da70d9f1476374bd42e09bee6
MarcoHernandez90 <marcohdez500@hotmail.com>
2021-12-21 11:11:59 -0700

    Hasta problemas de reactividad

--- src/api.js
+++ src/api.js
@@ -1,17 +1,20 @@
 const url = "https://api.coincap.io/v2";
 
+// Obtiene una lista de 20 criptomonedas
 function getAssets() {
   return fetch(`${url}/assets?limit=20`)
     .then((res) => res.json())
     .then((res) => res.data);
 }
 
+// Obtiene las criptomonedas de forma individual de acuerto a su id
 function getAsset(coin) {
   return fetch(`${url}/assets/${coin}`)
     .then((res) => res.json())
     .then((res) => res.data);
 }
 
+// Obtiene un historial de cambios en la criptomoneda en las últimas 24 hrs
 function getAssetHistory(coin) {
   const now = new Date();
   const end = now.getTime();
@@ -25,8 +28,24 @@ function getAssetHistory(coin) {
     .then((res) => res.data);
 }
 
+// Obtiene opciones de mercado para una criptomoneda
+function getMarkets(coin) {
+  return fetch(`${url}/assets/${coin}/markets?limit=5`)
+    .then((res) => res.json())
+    .then((res) => res.data);
+}
+
+// Obtiene el cambio de moneda desde los datos obtenidos con getMarkets
+function getExchange(id) {
+  return fetch(`${url}/exchanges/${id}`)
+    .then((res) => res.json())
+    .then((res) => res.data);
+}
+
 export default {
   getAssets,
   getAsset,
   getAssetHistory,
+  getMarkets,
+  getExchange,
 };
